bcb7692f33e7cd5d352db107e98dc8751450eacf
Andrew Miller <58837047+andrewmiller0728@users.noreply.github.com>
2020-11-06 03:21:13 -0500

    bounds

--- Programs/automata1.lua
+++ Programs/automata1.lua
@@ -83,7 +83,7 @@ local delay = 0.001
 for x = 1, w do
     local column = {}
     for y = 1, h do
-        if y > h / 2 - 10 and y < h / 2 + 10 and x > w / 2 - 10 and y < h / 2 - 10 then
+        if y > h / 2 - 10 and y < h / 2 + 10 and x > w / 2 - 10 and x < w / 2 + 10 then
             table.insert(column, 1)
         else
             table.insert(column, 0)
